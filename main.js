c930b972686e1b50f84ae488fc421bd109c00a8a
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2022-01-05 14:32:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function preload(){
     video = createVideo('video.mp4');
@@ -11,7 +12,36 @@ function setup(){
 }
 function draw() {
     image (video,0,0,480,380);
+
+    if (status != "") {
+        objectDetector.detect(video, gotResults);
+
+        for( i= 0 ; i < objects.length ; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "number of objects detected are:" + objects.length;
+
+            fill ("#FF0000");
+
+            percent = floor(objects[i].confidence * 100);
+            text ( objects[i].label + " " + percent +  "%",objects[i].x , objects[i].y );
+            noFill();
+            stroke ("#FF0000");
+            rect (objects[i].x - 15 , objects[i].y - 15 , objects[i].width , objects[i].height);
+
+        }
+    } 
+
 }
+
+function gotResults(error,results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
+
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "status: Detecting object";
